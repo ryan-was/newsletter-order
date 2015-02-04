2f7d908ad5c398f2dfac7da5315100bcfa52aeb0
Ryan Spencer <ryan.spencer@wearesocial.com.au>
2015-02-04 22:00:00 +1100

    allowing custom post_types to be set

--- custompostorder.php
+++ custompostorder.php
@@ -3,10 +3,10 @@
  * @package B&T Newsletter Order Up!
  */
 /*
-Plugin Name: B&T Newsletter Order
+Plugin Name: Newsletter Order
 Plugin URI: http://drewgourley.com/order-up-custom-ordering-for-wordpress/
 Description: Allows for the ordering of posts and custom post types through a simple drag-and-drop interface.
-Version: 2.2
+Version: 2.3
 Author: Drew Gourley
 Author URI: http://drewgourley.com/
 License: GPLv2 or later
@@ -28,8 +28,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-$custompostorder_defaults = array('post' => 0, 'per_page' => 12);
-$args = array( 'public' => true, '_builtin' => false ); 
+$custompostorder_defaults = array('post' => 0, 'per_page' => 12, 'post_type' => 'post');
+$args = array( 'public' => true, '_builtin' => false );
 $output = 'objects';
 $custompostorder_defaults = apply_filters('custompostorder_defaults', $custompostorder_defaults);
 $custompostorder_settings = get_option('custompostorder_settings');
@@ -39,6 +39,7 @@ add_action('admin_init', 'custompostorder_register_settings');
 function custompostorder_register_settings() {
     register_setting('custompostorder_settings', 'custompostorder_settings', 'custompostorder_settings_validate');
 }
+
 function custompostorder_update_settings() {
     global $custompostorder_settings, $custompostorder_defaults;
     if ( isset($custompostorder_settings['update']) ) {
@@ -46,12 +47,18 @@ function custompostorder_update_settings() {
             echo '<div class="error fade" id="message"><p>The Entries Per Page setting must be a positive integer, value reset to default.</p></div>';
             $custompostorder_settings['per_page'] = $custompostorder_defaults['per_page'];
         }
+
+        if ( strlen($custompostorder_settings['post_type']) == 0 ) {
+          $custompostorder_settings['post_type'] = $custompostorder_defaults['post_type'];
+        }
+
         $custompostorder_settings['per_page'] = min( 80, $custompostorder_settings['per_page'] );
         echo '<div class="updated fade" id="message"><p>Custom Post Order settings '.$custompostorder_settings['update'].'.</p></div>';
         unset($custompostorder_settings['update']);
         update_option('custompostorder_settings', $custompostorder_settings);
     }
 }
+
 function custompostorder_settings_validate($input) {
     $input['post'] = ($input['post'] == 1 ? 1 : 0);
     $args = array( 'public' => true, '_builtin' => false );
@@ -60,11 +67,11 @@ function custompostorder_settings_validate($input) {
     return $input;
 }
 
-function custompostorder_menu() {    
-    $args = array( 'public' => true, '_builtin' => false ); 
+function custompostorder_menu() {
+    $args = array( 'public' => true, '_builtin' => false );
     $output = 'objects';
-    add_menu_page(__('Newsletter Order'),  __('Newsletter Order'), 'edit_posts', 'custompostorder', 'custompostorder', plugins_url('images/post_order.png', __FILE__), 120); 
-    add_submenu_page('custompostorder', __('Order Newsletters'), __('Order Newsletters'), 'edit_posts', "custompostorder", 'custompostorder'); 
+    add_menu_page(__('Newsletter Order'),  __('Newsletter Order'), 'edit_posts', 'custompostorder', 'custompostorder', plugins_url('images/post_order.png', __FILE__), 120);
+    add_submenu_page('custompostorder', __('Order Newsletters'), __('Order Newsletters'), 'edit_posts', "custompostorder", 'custompostorder');
     add_posts_page(__('Order Newsletters', 'custompostorder'), __('Order Newsletters', 'custompostorder'), 'edit_posts', 'custompostorder', 'custompostorder');
 }
 
@@ -104,34 +111,34 @@ function custompostorder() {
     $options = $custompostorder_settings;
     if ( isset( $_GET['paged'] ) ) {
         $page = max( 1, $_GET['paged'] );
-    } else { 
+    } else {
         $page = 1;
     }
     $settings = '';
     $parent_ID = 0;
-    if ( $_GET['page'] == 'custompostorder' ) { 
-        $args = array( 'public' => true, '_builtin' => false ); 
+    if ( $_GET['page'] == 'custompostorder' ) {
+        $args = array( 'public' => true, '_builtin' => false );
         $output = 'objects';
         $settings .= '<input name="custompostorder_settings[post]" type="checkbox" value="1" ' . checked('1', $options['post'], false) . ' /> <label for="custompostorder_settings[post]">Check this box if you want to enable Automatic Sorting of all queries from this post type.</label>';
         $type_label = 'Newsletter';
         $type = 'post';
     } else {
-        $args = array( 'public' => true, '_builtin' => false ); 
+        $args = array( 'public' => true, '_builtin' => false );
         $output = 'objects';
         $settings .= '<input name="custompostorder_settings[post]" type="hidden" value="' . $options['post'] . '" />';
     }
-    if (isset($_POST['go-sub-posts'])) { 
+    if (isset($_POST['go-sub-posts'])) {
         $parent_ID = $_POST['sub-posts'];
     }
-    elseif (isset($_POST['hidden-parent-id'])) { 
+    elseif (isset($_POST['hidden-parent-id'])) {
         $parent_ID = $_POST['hidden-parent-id'];
     }
-    if (isset($_POST['return-sub-posts'])) { 
+    if (isset($_POST['return-sub-posts'])) {
         $parent_post = get_post($_POST['hidden-parent-id']);
         $parent_ID = $parent_post->post_parent;
     }
     $message = "";
-    if (isset($_POST['order-submit'])) { 
+    if (isset($_POST['order-submit'])) {
         custompostorder_update_order( $page );
     }
 ?>
@@ -139,12 +146,12 @@ function custompostorder() {
     <?php screen_icon('custompostorder'); ?>
     <h2><?php _e('Order ' . $type_label, 'custompostorder'); ?></h2>
     <form name="custom-order-form" method="post" action="">
-        <?php  
+        <?php
         $args = array(
             'orderby' => 'menu_order',
             'order' => 'ASC',
-            'post_parent' => $parent_ID, 
-            'post_type' => $type,
+            'post_parent' => $parent_ID,
+            'post_type' => $options['post_type'],
             'posts_per_page' => $options['per_page'],
             'paged' => $page,
             'category_name' => 'newsletter',
@@ -185,8 +192,8 @@ function custompostorder() {
                     'prev_next' => false,
                     'current' => $page,
                     'total' => $query->max_num_pages
-                    ); 
-                $pagination = paginate_links($args); 
+                    );
+                $pagination = paginate_links($args);
                 if ( !empty($pagination) ) { ?>
                 <div class="misc-pub-section">
                     <div class="tablenav" style="margin:0">
@@ -218,7 +225,7 @@ function custompostorder() {
                     </select>
                     <input type="submit" name="go-sub-posts" class="button" id="go-sub-posts" value="<?php _e('Order Sub-posts', 'custompostorder') ?>" />
                 </div>
-            </div>        
+            </div>
             <?php } ?>
         </div>
         <?php } else { ?>
@@ -233,6 +240,9 @@ function custompostorder() {
             </tr>
             <tr valign="top"><th scope="row">Entries Per Page</th>
             <td><input name="custompostorder_settings[per_page]" type="text" value="<?php echo $options['per_page']; ?>" style="width:48px" /></td>
+            </tr>
+            <tr valign="top"><th scope="row">Post Type</th>
+            <td><input name="custompostorder_settings[post_type]" type="text" value="<?php echo $options['post_type']; ?>" style="width:48px" /></td>
             </tr>
         </table>
         <input type="hidden" name="custompostorder_settings[update]" value="Updated" />
@@ -251,10 +261,10 @@ function custompostorder() {
         });
     });
     function custompostorderAddLoadEvent(){
-        jQuery("#custom-order-list").sortable({ 
-            placeholder: "sortable-placeholder", 
+        jQuery("#custom-order-list").sortable({
+            placeholder: "sortable-placeholder",
             revert: false,
-            tolerance: "pointer" 
+            tolerance: "pointer"
         });
     };
     addLoadEvent(custompostorderAddLoadEvent);
@@ -271,7 +281,7 @@ function custompostorder() {
 function custompostorder_update_order( $page = 1 ) {
     global $custompostorder_settings;
     $options = $custompostorder_settings;
-    if ( isset( $_POST['hidden-custom-order'] ) && $_POST['hidden-custom-order'] != '' ) { 
+    if ( isset( $_POST['hidden-custom-order'] ) && $_POST['hidden-custom-order'] != '' ) {
         if ( $page > 2 ) {
             $offset = ( $page - 1 ) * $options['per-page'];
         } else {
@@ -293,12 +303,12 @@ function custompostorder_update_order( $page = 1 ) {
 }
 function custompostorder_sub_query( $query, $type ) {
     $options = '';
-    while ( $query->have_posts() ) : $query->the_post(); 
+    while ( $query->have_posts() ) : $query->the_post();
         $page_ID = get_the_ID();
         $args = array( 'post_parent' => $page_ID, 'post_type' => $type );
         $subpages = new WP_Query( $args );
         if ( $subpages->have_posts() ) {
-            $options .= '<option value="' . $page_ID . '">' . get_the_title($page_ID) . '</option>'; 
+            $options .= '<option value="' . $page_ID . '">' . get_the_title($page_ID) . '</option>';
         }
     endwhile;
     return $options;
